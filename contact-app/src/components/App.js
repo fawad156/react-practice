bfb8e5e5dbd6cac61d1d2c8992060e41a66afee1
Fawad <fawad.ahsan22@gmail.com>
2022-07-19 14:43:04 +0500

    add image avatar & useState

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 //import ContactCard from './ContactCard';
 import ContactList from './ContactList';
 function App() {
+  
+  const [contacts, setContacts]=useState([]);
+
+  const addContactHandler = (contact)=>{
+    console.log(contact);
+    setContacts([...contacts, contact])
+  };
   const contact_list=[
     {
       id: "1",
@@ -25,8 +32,8 @@ function App() {
   return (
     <div className='ui container'>
       <Header />
-      <AddContact />
-      <ContactList contacts={contact_list}/>
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
