7ada6b57374f5f71d710b3201cdce12624ac1ec8
Fawad <fawad.ahsan22@gmail.com>
2022-07-20 18:46:20 +0500

    add routes to render components

--- contact-app/src/components/ContactList.js
+++ contact-app/src/components/ContactList.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import ContactCard from './ContactCard';
 
 const ContactList=(props)=>{
     console.log("props",props);
+
     const deleteContactHandler=(id)=>{
         props.getContactId(id)
     };
+
     const renderContactList=props.contacts.map((contact)=>{
         return(
             <ContactCard contact={contact} clickHandler={deleteContactHandler}></ContactCard>
         )
-    })
-    return(
-        <div className='ui celled list'>{renderContactList}</div>
-    );
+    });
+    return (
+        <div className="main">
+          <div>
+          <h2>
+            Contact List
+            <Link to="/add">
+              <button className="ui button blue right">Add Contact</button>
+            </Link>
+          </h2>
+          </div>
+          <div className="ui celled list">{renderContactList}</div>
+        </div>
+      );
 }
 
 export default ContactList;
